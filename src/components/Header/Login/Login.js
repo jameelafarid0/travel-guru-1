b119ef5b3c949b93f564f0fab12370060a243f12
samia07farid <samiafarid07@gmail.com>
2020-09-19 10:34:44 -0700

    working on form design

--- src/components/Header/Login/Login.js
+++ src/components/Header/Login/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from '../../../firebase.config';
-import { InfoOutlined } from '@material-ui/icons';
+import './Login.css'
 
 firebase.initializeApp(firebaseConfig);
 
@@ -79,6 +79,7 @@ const Login = () => {
                 newUserInfo.error = '';
                 newUserInfo.success = true;
                 setUser(newUserInfo);
+                updateUserName(user.name);
             })
             .catch(error => {
                 const newUserInfo = {...user};
@@ -95,6 +96,7 @@ const Login = () => {
                 newUserInfo.error = '';
                 newUserInfo.success = true;
                 setUser(newUserInfo);
+                console.log('updated name', res.user)
             })
             .catch(function(error) {
                 const newUserInfo = {...user};
@@ -104,37 +106,60 @@ const Login = () => {
               });
         }
 
+        const updateUserName = name => {
+            var user = firebase.auth().currentUser;
+
+            user.updateProfile({
+            displayName: name,
+            }).then(function() {
+            // Update successful.
+            }).catch(function(error) {
+            // An error happened.
+});
+        }
+
         e.preventDefault();
     }
  
     return (
-        <div>
-            <h1>this is login</h1>
+        <div className="main-container">
+            <div className="sub-container">
+                <div className="form-title">
+                {newUser ? <h3>Create an account</h3> : <h3>Log In</h3>}
+                </div>
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        {newUser && <input type="text" name="name" onBlur={handleBlur} placeholder="Username" className="input" required/>}
+                        <br/>
+                        <input type="text" name="email" onBlur={handleBlur} placeholder="Email" className="input" required/>
+                        <br/>
+                        <input type="password" name="password" onBlur={handleBlur} placeholder="Password" className="input" required/>
+                        <br/>
+                        <input className="input-btn" type="submit" value={newUser ? "Create an account" : "Login"}/>
+                    </form>
+                </div>
+                <div className="toggle-style">
+                {
+                    newUser
+                    ?
+                    <> <span >Already have an account?</span> <button className="button" type="button" onClick={() => setNewUser(!newUser)}>Login</button> </>
+                    :
+                    <> <span>Don't have an account?</span> <button className="button" type="button" onClick={() => setNewUser(!newUser)}>Create an account</button> </>
+                }
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <span className="or-style">OR</span>
             {user.isSignedIn ? <button onClick={handleSignOut} >Sign out</button> :
                 <button onClick={handleSignIn} >Sign in</button>}
-            {
-                user.isSignedIn && <p>Welcome, {user.name}</p>
-            }
-
-            <div>
-                <p>Email: {user.email}</p>
-                <p>Name: {user.name}</p>
-                <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id=""/>
-                <label htmlFor="newUser">New user sign up</label>
-                <form onSubmit={handleSubmit}>
-                    {newUser && <input type="text" name="name" onBlur={handleBlur} placeholder="Username" required/>}
-                    <br/>
-                    <input type="text" name="email" onBlur={handleBlur} placeholder="Email" required/>
-                    <br/>
-                    <input type="password" name="password" onBlur={handleBlur} placeholder="Password" required/>
-                    <br/>
-                    <input type="submit" value="Create an account"/>
-                </form>
-            </div>
-            <p style={{color: 'red'}}>{user.error}</p>
-            {user.success && <p style={{color: 'green'}}>user {newUser ? 'created' : 'logged in'} successfully</p>}
+            
+            
+            {/* <p style={{color: 'red'}}>{user.error}</p>
+            {user.success && <p style={{color: 'green'}}>user {newUser ? 'created' : 'logged in'} successfully</p>} */}
         </div>
     );
 };
 
 export default Login;
+
